e313c72d252013e6a3584c3abb693cf1c04286d7
TonyHazinski <70490786+TonyHazinski@users.noreply.github.com>
2022-07-27 23:55:56 -0500

    Add files via upload

---
+++ TicTacToe.js
@@ -0,0 +1,89 @@
+const cells = document.querySelectorAll(".cell");
+let statusDisplay = document.getElementById("status");
+let currentPlayer = "X";
+let options = ["", "", "", "", "", "", "", "", ""];
+let turns = 0;
+let gameRunning = false;
+const winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [0, 4, 8]
+];
+
+function intitialzeGame() {
+    console.log("Initializing...")
+    cells.forEach(cell => {
+        cell.onclick = cellClicked;
+        cell.onmouseover = cellMouseOver;
+        cell.onmouseleave = cellMouseLeave;
+    });
+    gameRunning = true;
+
+}
+
+function cellClicked() {
+    if (!gameRunning || options[this.attributes.cellindex.value] != "") {
+        return;
+    }
+    turns += 1;
+    this.style.color = 'black';
+    this.innerText = currentPlayer;
+    options[this.attributes.cellindex.value] = currentPlayer;
+    checkGameStatus()
+    currentPlayer = currentPlayer == "X" ? "O" : "X";
+}
+
+function cellMouseOver() {
+    if (!gameRunning) {
+        return;
+    }
+    if (options[this.attributes.cellindex.value] == "") {
+        this.innerText = currentPlayer;
+    }
+}
+
+function cellMouseLeave() {
+    if (!gameRunning) {
+        return;
+    }
+    if (options[this.attributes.cellindex.value] == "") {
+        this.innerText = "";
+    }
+}
+
+function checkGameStatus() {
+    winConditions.forEach(condition => {
+        if (options[condition[0]] == "") { //Make sure it's not an empty set
+            return;
+        }
+        if (options[condition[0]] == options[condition[1]] && options[condition[1]] == options[condition[2]]) {
+            statusDisplay.innerText = currentPlayer + " wins!"
+            gameRunning = false;
+        }
+    })
+    if (turns == 9 && gameRunning) {
+        statusDisplay.innerText = "Its a tie!"
+        gameRunning = false;
+    }
+}
+
+function restart() {
+    cells.forEach(cell => {
+        cell.innerText = "";
+        cell.style.color = "lightgray";
+    })
+    for (let i = 0; i < 9; i++) {
+        options[i] = "";
+    }
+    gameRunning = true;
+    statusDisplay.innerText = ""
+    currentPlayer = "X"
+    turns = 0;
+}
+
+intitialzeGame();
